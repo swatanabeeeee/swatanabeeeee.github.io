63f66ceee67bba721dbfc4a69da4db9ec565b8c7
swatanabeeeee <84706685+swatanabeeeee@users.noreply.github.com>
2021-10-13 10:50:02 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
-      bottomBar: {
+      toolbar: {
         title: 'Sticker to shapes',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
