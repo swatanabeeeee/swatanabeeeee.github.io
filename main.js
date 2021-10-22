978e8cf75644d875d27bf786c4594b6b7cdc1c49
swatanabeeeee <84706685+swatanabeeeee@users.noreply.github.com>
2021-10-22 15:07:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ miro.onReady(() => {
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
         onClick: async () => {
+          console.log("aaa")
           // Get selected widgets
           let selectedWidgets = await miro.board.selection.get()
 
