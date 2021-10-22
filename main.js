e10f8d07e55f729c3fbd72ac742a6ddef9630d2a
swatanabeeeee <swatanabe@mxdlab.net>
2021-10-22 17:47:56 +0900

    宝3

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
       bottomBar: {
-        title: 'treasure4',
+        title: 'treasure5',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
@@ -11,10 +11,10 @@ miro.onReady(() => {
           let selectedWidgets = await miro.board.selection.get()
 
           // Filter stickers from selected widgets
-          let treasure = selectedWidgets.filter((widget) => widget.id === '3074457366196671495')
+          // let treasure = selectedWidgets.filter((widget) => widget.id === '3074457366196671495')
 
           // Delete selected stickers
-          // await miro.board.widgets.deleteById(selectedWidgets.map((widget) => widget.id))
+          await miro.board.widgets.deleteById(selectedWidgets.map((widget) => widget.id))
 
           // Create shapes from selected stickers
           await miro.board.widgets.create(
