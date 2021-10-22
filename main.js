deb0aac31e295f89528523548285b84a6430a2bc
swatanabeeeee <swatanabe@mxdlab.net>
2021-10-22 18:16:12 +0900

    宝3

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
       bottomBar: {
-        title: 'treasure5',
+        title: 'treasure6',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
@@ -34,7 +34,7 @@ miro.onReady(() => {
       },
 
       bottomBar: {
-        title: 'treasure5',
+        title: 'treasure7',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
