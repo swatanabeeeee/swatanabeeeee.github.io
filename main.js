a6c26a88020306a80199d388216e210011427187
swatanabeeeee <swatanabe@mxdlab.net>
2021-10-25 11:27:42 +0900

    宝3

--- main.js
+++ main.js
@@ -2,38 +2,6 @@ miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
       bottomBar: {
-        title: 'treasure6',
-        svgIcon:
-          '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
-        positionPriority: 1,
-        onClick: async () => {
-          // Get selected widgets
-          let selectedWidgets = await miro.board.selection.get()
-
-          // Filter stickers from selected widgets
-          // let treasure = selectedWidgets.filter((widget) => widget.id === '3074457366196671495')
-
-          // Delete selected stickers
-          await miro.board.widgets.deleteById(selectedWidgets.map((widget) => widget.id))
-
-          // Create shapes from selected stickers
-          await miro.board.widgets.create(
-            selectedWidgets.map((widget) => ({
-              type: 'shape',
-              text: "aaa",
-              x: widget.x,
-              y: widget.y,
-              width: 100,
-              height: 100,
-            })),
-          )
-
-          // Show success message
-          miro.showNotification('Stickers has been converted')
-        },
-      },
-
-      bottomBar: {
         title: 'treasure7',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
