98c604651253cfe5fb6bcc8a0b2a2a635dbeb2f4
swatanabeeeee <84706685+swatanabeeeee@users.noreply.github.com>
2022-01-12 12:40:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ miro.onReady(() => {
 
           // Show success message
 //           miro.showNotification(selectedWidgets)
-          console.log(selectedWidgets.style)
+          console.log(selectedWidgets[0])
         },
       },
     },
