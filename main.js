8fd37070442021c9623f1c46ff563c901a204a14
swatanabeeeee <swatanabe@mxdlab.net>
2022-01-12 14:53:19 +0900

    練習

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ async function getWidget() {
   let objects = await miro.board.widgets.get({id: '3458764516401601937'})
   // let backWidget = objects.filter((widget) => widget.id === '3458764516401601937')
   console.log(objects[0].style.backgroundColor)
-  miro.board.widgets.update(
-    objects[0].text = "おめでとうございます"
+  objects.update(
+    {text : "おめでとうございます"},
   )
 
 
