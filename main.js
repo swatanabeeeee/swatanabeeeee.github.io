1a1265ca6865c04c1352cbf084fb3704c7e1ef01
swatanabeeeee <84706685+swatanabeeeee@users.noreply.github.com>
2022-01-12 10:46:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
       bottomBar: {
-        title: 'sample4',
+        title: 'sample5',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
@@ -30,7 +30,7 @@ miro.onReady(() => {
             colortext = "#8fd14f"
           }
           
-          await miro.board.widgets.update(
+          await selectedWidgets.update(
             selectedWidgets.map((widget) => ({
               style: {
                   backgroundColor : colortext,
