0ef701020283252c8d6ccb0179f594f3659027f6
swatanabeeeee <84706685+swatanabeeeee@users.noreply.github.com>
2022-01-12 09:42:42 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
       bottomBar: {
-        title: 'treasure8',
+        title: 'sample1',
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
