d374618a26727fe2c4cdd02eeea9eb2830dc5787
swatanabeeeee <swatanabe@mxdlab.net>
2022-02-03 10:27:55 +0900

    finish

--- main.js
+++ main.js
@@ -43,8 +43,8 @@ async function Authorization() {
 async function ChangeLocation(date) {
   today = (date.getMonth()+1) + "月" + date.getDate() + "日";
   console.log(today)
-  // console.log("情報", kitchenData[today])
-  console.log("情報2", kitchenData["2月3日"])
+  console.log("情報", kitchenData[today])
+  // console.log("情報2", kitchenData["2月3日"])
 
 }
 
