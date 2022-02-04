c1bdd6b5c895f6bdf3b04fa0678960c75aa80ba7
swatanabeeeee <swatanabe@mxdlab.net>
2022-02-04 09:53:03 +0900

    メッセージ

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const ID_daikichi = 3458764518031867948;
 
 miro.onReady(() => {
   Authorization();
-  console.log(miro.board.getInfo())
+  console.log(miro.board.getInfo().id)
 })
 
 async function Authorization() {
