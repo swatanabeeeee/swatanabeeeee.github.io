f5c8308b70719135c5e42c139843228b2df2146c
swatanabeeeee <swatanabe@mxdlab.net>
2022-02-24 10:46:36 +0900

    homepage

--- kitchenData.js
+++
@@ -1,7 +0,0 @@
-const ExportKitchenData = () => {
-  const data = [{"12月1日": {"A": "ウッキー,トンちゃんくん", "B": "Daikichi", "C": "", "D": "吉吾", "E": "キッチンカー・アド", "F": "いか焼きみなせん", "G": "Kyotoケバブ屋", "H": "鉄平", "I": "じぃじの餃子", "J": "MONKAZI", "K": "T-STAR"}}];
-  
-      return data;
-  };
-  
-  export default ExportKitchenData;
